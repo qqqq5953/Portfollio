2a9e6e65806180c49168d95580113eccaf8bffb4
AndyHsieh <andy.hsieh@gaiatechs.com>
2025-07-26 15:46:02 +0800

    add tanstack query

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,5 +2,20 @@ import { createApp } from "vue";
 import "./index.css";
 import App from "./App.vue";
 import router from "./router";
+import { QueryClient, VueQueryPlugin } from '@tanstack/vue-query'
 
-createApp(App).use(router).mount("#app");
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      select: (data: any) => {
+        console.log("select data", data);
+        return data.data
+      },
+      // optionally add other defaults
+      staleTime: 5 * 60 * 1000,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+createApp(App).use(VueQueryPlugin, { queryClient }).use(router).mount("#app");
